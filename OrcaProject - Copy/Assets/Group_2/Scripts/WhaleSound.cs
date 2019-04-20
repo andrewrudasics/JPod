ebfd5ac068ad397c56414cafb70531d1664c4251
Greg Guo <grgrggtr@qq.com>
2019-04-19 17:14:35 -0700

    4.19 afternoon

---
+++ OrcaProject - Copy/Assets/Group_2/Scripts/WhaleSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleSound : MonoBehaviour {
+    public AudioClip[] clips;
+    public float maxGap;
+    public float minGap;
+
+   
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(PlaySound());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator PlaySound()
+    {
+        yield return new WaitForSeconds(Random.Range(minGap, maxGap));
+        while (true)
+        {
+            print("playering");
+            GetComponent<AudioSource>().PlayOneShot(clips[(int)Random.Range(0, clips.Length)]);
+            yield return new WaitForSeconds(Random.Range(minGap, maxGap));
+        }
+    }
+}
